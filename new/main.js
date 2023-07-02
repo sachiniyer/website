6ac5fb17fd4e133671c61f8af18fabbbc5272f42
Sachin Iyer <sachinjiyer@gmail.com>
2023-07-02 15:54:41 -0700

    consume status.sachiniyer.com for status info

--- new/main.js
+++ new/main.js
@@ -30,10 +30,32 @@ function resizeColumnLists() {
   }
 }
 
-window.addEventListener('load', resizeColumnLists);
-window.addEventListener('resize', resizeColumnLists);
-window.addEventListener('orientationchange', resizeColumnLists);
+function createElements() {
+  createInnerElements();
+  createOuterElements();
+  createSubElements();
+}
 
+function createElement(elem, key, value) {
+  var li = document.createElement('li');
+  var a = document.createElement('a');
+  var p = document.createElement('p');
+
+  li.appendChild(a);
+  a.appendChild(p);
+
+  a.setAttribute('class', 'btn');
+  a.setAttribute('href', value);
+  a.setAttribute('type', 'button');
+
+  p.setAttribute('class', 'general-text responsive-text');
+  let color = getRandomColor();
+  p.style.setProperty('--hover-color', color);
+  p.innerHTML = key;
+
+  elem.appendChild(li);
+
+}
 
 
 function createInnerElements() {
@@ -41,23 +63,7 @@ function createInnerElements() {
 
   for (var key in inner_web) {
     var value = inner_web[key];
-    var li = document.createElement('li');
-    var a = document.createElement('a');
-    var p = document.createElement('p');
-
-    li.appendChild(a);
-    a.appendChild(p);
-
-    a.setAttribute('class', 'btn');
-    a.setAttribute('href', value);
-    a.setAttribute('type', 'button');
-
-    p.setAttribute('class', 'general-text responsive-text');
-    let color = getRandomColor();
-    p.style.setProperty('--hover-color', color);
-    p.innerHTML = key;
-
-    innerElements.appendChild(li);
+    createElement(innerElements, key, value)
   }
 }
 
@@ -66,89 +72,16 @@ function createOuterElements() {
 
   for (var key in outer_web) {
     var value = outer_web[key];
-    var li = document.createElement('li');
-    var a = document.createElement('a');
-    var p = document.createElement('p');
-
-    li.appendChild(a);
-    a.appendChild(p);
-
-    a.setAttribute('class', 'btn');
-    a.setAttribute('href', value);
-    a.setAttribute('type', 'button');
-
-    p.setAttribute('class', 'general-text responsive-text');
-    p.style.setProperty('--hover-color', getRandomColor());
-    p.innerHTML = key;
-
-    outerElements.appendChild(li);
+    createElement(outerElements, key, value)
   }
 }
 
-
-function findSubElements() {
-  return new Promise(function (resolve, _) {
-    let request = new XMLHttpRequest();
-    request.open('GET', 'https://status.sachiniyer.com', true);
-    request.send(null);
-    request.onreadystatechange = function () {
-      if (request.readyState === 4 && request.status === 200) {
-        let type = request.getResponseHeader('Content-Type');
-        if (type.indexOf("text") !== 1) {
-          let sub_web = request.responseText;
-          let sub_web_json = JSON.parse(sub_web);
-          resolve(Object.keys(sub_web_json))
-        }
-      }
-    }
-  });
-}
-
-function testSite(url) {
-  return new Promise(function (resolve, _) {
-    let request = new XMLHttpRequest();
-    // allow cors
-    request.withCredentials = true;
-    request.open('GET', url, true);
-    request.send(null);
-    request.onreadystatechange = function () {
-      if (request.readyState === 4 && request.status === 200) {
-        resolve(true);
-      } else {
-        resolve(false);
-      }
-    }
-  });
-}
-
-
-function createSubElements(elems) {
+function createSubElements() {
   var subElements = document.getElementById('subElements');
-
-  for (var key of elems) {
-    let url = 'http://' + key;
-    testSite(url)
-      .then(function (f) {
-        if (f) {
-          var li = document.createElement('li');
-          var a = document.createElement('a');
-          var p = document.createElement('p');
-
-          p.style.setProperty('--hover-color', getRandomColor());
-          li.appendChild(a);
-          a.appendChild(p);
-
-          a.setAttribute('class', 'btn');
-          a.setAttribute('href', url);
-          a.setAttribute('type', 'button');
-
-          p.setAttribute('class', 'general-text responsive-text');
-          p.innerHTML = key;
-
-          subElements.appendChild(li);
-        }
-      });
-  }
+  let socket = new WebSocket("wss://status.sachiniyer.com/ws");
+  socket.onmessage = function (event) {
+    createElement(subElements, event.data.substring(8), event.data)
+  };
 }
 
 function toggleDisplay(e) {
@@ -167,14 +100,9 @@ function toggleDisplay(e) {
 
 }
 
-function createElements() {
-  createInnerElements();
-  createOuterElements();
-  findSubElements()
-    .then(function (f) {
-      createSubElements(f);
-    })
-}
+
+window.addEventListener('load', resizeColumnLists);
+window.addEventListener('resize', resizeColumnLists);
+window.addEventListener('orientationchange', resizeColumnLists);
 
 window.addEventListener('load', createElements);
-
