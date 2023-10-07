31d7110d081ef800a7a89eb34b520e14d22736fc
Sachin Iyer <sachinjiyer@gmail.com>
2023-10-07 15:23:02 -0700

    sort projects based off main resume

--- projects/main.js
+++ projects/main.js
@@ -1,18 +1,23 @@
 let PROJECTS_API = "https://api.github.com/repos/sachiniyer/resume/contents/projects?ref=master";
+let RESUME_LINK = "https://raw.githubusercontent.com/sachiniyer/resume/master/resume.tex"
 
 async function createElements() {
   let projectsRaw = await getProjectsRaw();
   let projects = parseProjects(projectsRaw);
-  addProjects(projects);
+  let order = await getOrder(projects);
+  let sorted_projects = sortProjects(projects, order);
+  addProjects(sorted_projects);
 }
 
 async function getProjectsRaw() {
   let urls = [];
+  let names = [];
   await fetch(PROJECTS_API)
     .then(response => response.json())
     .then(function (data) {
       for (let i = 0; i < data.length; i++) {
         urls.push(data[i].download_url);
+        names.push(data[i].name.slice(0, -4))
       }
     });
   let res = [];
@@ -22,7 +27,7 @@ async function getProjectsRaw() {
       fetch(urls[i])
         .then(response => response.text())
         .then(function (data) {
-          res.push(data);
+          res.push({ "data": data, "name": names[i] });
         }));
   }
   await Promise.all(futures);
@@ -30,28 +35,51 @@ async function getProjectsRaw() {
 }
 
 function parseProjects(raw) {
-  let parsed = fixHref(raw);
-  for (let i = 0; i < parsed.length; i++) {
-    let str = parsed[i].split("{");
-    let title = str[1];
-    let desc = str[2]
-    parsed[i] = {
-      title: title.trim().split("}")[0],
-      desc: desc.trim().split("}")[0]
-    }
-  }
-  return parsed;
-}
-
-function fixHref(raw) {
   let res = [];
   for (let i = 0; i < raw.length; i++) {
-    let str = raw[i];
-    const convertedString = str.replace(/\\href\{(.*?)\}\{(.*?)\}/g, (_, url, text) => {
-      return `<a href="${url}">${text.trim()}</a>`;
+    let parsed = fixHref(raw[i]["data"]);
+    let str = parsed.split("{");
+    let title = str[1];
+    let desc = str[2]
+    res.push({
+      title: title.trim().split("}")[0],
+      desc: desc.trim().split("}")[0],
+      name: raw[i]["name"]
     });
-    res.push(convertedString);
   }
+  return res;
+}
+
+function fixHref(str) {
+  return str.replace(/\\href\{(.*?)\}\{(.*?)\}/g, (_, url, text) => {
+    return `<a href="${url}">${text.trim()}</a>`;
+  });
+}
+
+async function getOrder(names) {
+  let res = [];
+  await fetch(RESUME_LINK)
+    .then(response => response.text())
+    .then(function (data) {
+      let lines = data.split("\n");
+      for (let i = 0; i < lines.length; i++) {
+        let line = lines[i];
+        if (line.includes("./projects/")) {
+          res.push(line.split("./projects/")[1].split("}")[0]);
+        }
+      }
+    });
+  return res;
+}
+
+function sortProjects(projects, order) {
+  let res = [];
+  for (let name in order) {
+    let index = projects.findIndex(x => x.name === order[name]);
+    res.push(projects[index]);
+    projects.splice(index, 1);
+  }
+  res = res.concat(projects);
   return res;
 }
 
@@ -71,6 +99,5 @@ function addProjects(projects) {
     container.append(add)
   }
 }
-
 
 window.addEventListener('load', createElements);
