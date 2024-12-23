e8ffa359ed5a505c1bed8dfd04af9b5b139b0b7d
Sachin Iyer <sachinjiyer@gmail.com>
2024-12-22 21:49:49 -0800

    add timeout for matomo

--- packages/matomo.js
+++ packages/matomo.js
@@ -1,11 +1,32 @@
-var _paq = window._paq = window._paq || [];
+var _paq = (window._paq = window._paq || []);
 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-_paq.push(['trackPageView']);
-_paq.push(['enableLinkTracking']);
+_paq.push(["trackPageView"]);
+_paq.push(["enableLinkTracking"]);
 (function () {
   var u = "//track.sachiniyer.com/";
-  _paq.push(['setTrackerUrl', u + 'matomo.php']);
-  _paq.push(['setSiteId', '1']);
-  var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
-  g.async = true; g.src = u + 'matomo.js'; s.parentNode.insertBefore(g, s);
+  _paq.push(["setTrackerUrl", u + "matomo.php"]);
+  _paq.push(["setSiteId", "1"]);
+  var d = document,
+    g = d.createElement("script"),
+    s = d.getElementsByTagName("script")[0],
+    timeout = setTimeout(function () {
+      console.warn("Matomo script loading timed out.");
+      if (g && g.parentNode) {
+        g.parentNode.removeChild(g);
+      }
+    }, 1000); // 1 second timeout
+
+  g.async = true;
+  g.src = u + "matomo.js";
+
+  g.onload = function () {
+    clearTimeout(timeout); // Clear the timeout if the script loads successfully
+  };
+
+  g.onerror = function () {
+    clearTimeout(timeout);
+    console.error("Matomo script failed to load.");
+  };
+
+  s.parentNode.insertBefore(g, s);
 })();
